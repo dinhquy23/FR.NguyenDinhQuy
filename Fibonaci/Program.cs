36c8a004f24b153ebbb10805bdfb4a3ee5538f51
victory23121999 <victory23121999@gmail.com>
2022-08-02 10:48:25 +0700

    Them

---
+++ Fibonaci/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Fibonaci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine($"Nhap so n: ");
+            n=int.Parse(Console.ReadLine());
+            DisplayFibonaci(n);
+            Console.ReadKey();
+        }
+
+        private static void DisplayFibonaci(int n)
+        {
+            if(n==1)
+            {
+                Console.WriteLine($"1");
+            }
+            else if(n==2)
+            {
+                Console.WriteLine($"1,  1");
+            }
+            else
+            {
+                for(int i = 0; i < n; i++)
+                {
+
+                }
+            }
+        }
+    }
+}
